694af2e34c7483b97dbd36467b63d4266f549293
lucaspires-source <pires.lucas94@gmail.com>
2021-12-04 12:37:58 -0300

    finalizing user Login

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -3,12 +3,42 @@ const bcrypt = require('bcryptjs');
 const jwt = require ('jsonwebtoken')
 const { SECRET_KEY } = require('../../config')
 const { UserInputError} = require('apollo-server') 
-const {validadeRegisterInput} = require('../../util/validators')
+const {validadeRegisterInput,validateLoginInput} = require('../../util/validators')
+
+function generateToken(user){
+    return jwt.sign({
+        id:user.id,
+        email: user.email,
+        username: user.username
+    }, SECRET_KEY, {expiresIn:'1h'})
+}
 module.exports = {
     Mutation : {
-        async register( _ , {registerInput: {username, email, password, confirmPassword}}, context, info)
+        async login(_,{ username,password}){
+            const {valid, errors} = validateLoginInput(username,password)
+            if(!valid) {
+                throw new UserInputError('errors', {errors})
+            } 
+            const user =  await User.findOne({username})
+            if(!user){
+                errors.general = 'User Not Found'
+                throw new UserInputError('User Not Found', {errors})
+            }
 
-        {
+            const match = await bcrypt.compare(password, user. password)
+            if(!match){
+                errors.general = 'Wrong Credentials'
+                throw new UserInputError('Wrong Credentials', {errors})
+            }
+
+             const token =  generateToken(user)
+             return {
+                ...user._doc,
+                id:user._id,
+                token
+            }
+        },
+        async register( _ , {registerInput: {username, email, password, confirmPassword}}, context, info){
             const {valid, errors} = validadeRegisterInput(username,email,password, confirmPassword)
             if(!valid) {
                 throw new UserInputError('errors', {errors})
@@ -32,17 +62,14 @@ module.exports = {
             })
             const res = await newUser.save()
 
-        const token = jwt.sign({
-            id:res.id,
-            email: res.email,
-            username: res.username
-        }, SECRET_KEY, {expiresIn:'1h'})
+            const token =  generateToken(res)
+           
+            
             return {
                 ...res._doc,
                 id:res._id,
                 token
             }
         }
-
     }
 }
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -28,5 +28,6 @@ type Query {
 
 type Mutation {
     register(registerInput: RegisterInput):User!
+    login(username:String!, password:String!):User!
 }
 `
