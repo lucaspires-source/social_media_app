816d971121c6503c4d4034cf8a2148a1154daf1c
lucaspires-source <pires.lucas94@gmail.com>
2021-12-07 14:40:09 -0300

    create post mutation finished

--- graphql/resolvers/post.js
+++ graphql/resolvers/post.js
@@ -1,5 +1,5 @@
 const Post = require('../../models/Post')
-
+const checkAuth = require('../../util/check-auth')
 
 module.exports = {
 
@@ -26,6 +26,18 @@ module.exports = {
         },
     },
     Mutation: {
-        async createPost(_, { body })
+        async createPost(_, { body }, context){
+            const user = checkAuth(context)
+            console.log(user)
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            })
+            const post = await newPost.save()
+
+            return post
+        }
     }
 }
