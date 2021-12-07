85ab188608f35436917ae6e21e4c0fbfccfa9f74
lucaspires-source <pires.lucas94@gmail.com>
2021-12-07 13:42:00 -0300

    prototype for new post mutation

--- graphql/resolvers/post.js
+++ graphql/resolvers/post.js
@@ -11,6 +11,21 @@ module.exports = {
             } catch(err){
                 throw new Error(err)
             }
-        }
+        },
+        async getPost(_, {postId}){
+            try{
+                const post = await Post.findById(postId)
+                if(post){
+                    return post
+                } else {
+                    throw new Error ('Post not Found')
+                }
+            }catch (err){
+                throw new Error(err)
+            }
+        },
+    },
+    Mutation: {
+        async createPost(_, { body })
     }
 }
