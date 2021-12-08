4d414037f1c91fbfeb72a150918a1b8e2f81a30b
lucaspires-source <pires.lucas94@gmail.com>
2021-12-08 14:08:32 -0300

    creating neww type defs

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -8,11 +8,25 @@ type Post {
     body:String!
     username:String!
     createdAt:String!
+    comments: [Comment!]!
+    likes:[Like]!
 }
 type User {
     id:ID!
     email:String!
     token:String!
+    username:String!
+    createdAt:String!
+}
+type Comment {
+    id:ID!
+    body:String!
+    username:String!
+    createdAt:String!
+}
+
+type Like {
+    id:ID!
     username:String!
     createdAt:String!
 }
